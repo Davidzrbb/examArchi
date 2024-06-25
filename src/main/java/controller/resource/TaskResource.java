cfde8176b40d1b7570432f0c59d81ad10ececd35
QuentinCoqueran <coqueranquentin@gmail.com>
2024-06-25 18:22:48 +0200

    task command resource wip

--- src/main/java/controller/resource/TaskResource.java
+++ src/main/java/controller/resource/TaskResource.java
@@ -1,6 +1,28 @@
 package controller.resource;
+import org.apache.commons.cli.*;
 
 public class TaskResource {
+    public static Options createOptions() {
+        Options options = new Options();
+        options.addOption("add", "verbose", false, "Enable verbose mode");
+        options.addOption("delete", "input", true, "Specify input file");
+        options.addOption("list", "output", true, "Specify output file");
+        return options;
+    }
 
+    public static void dispatch(String[] args, Options options) {
 
+        CommandLineParser parser = new DefaultParser();
+        try {
+            CommandLine cmd = parser.parse(options, args);
+            boolean verbose = cmd.hasOption("verbose");
+            String inputFile = cmd.getOptionValue("input");
+
+            // Continue with processing...
+
+        } catch (ParseException e) {
+            System.err.println("Error: " + e.getMessage());
+            System.exit(1);
+        }
+    }
 }
