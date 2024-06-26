7a9dd21aed037de6592e4daa77599691a65620d6
David <zerbib.david.1@gmail.com>
2024-06-26 17:11:32 +0200

    Auto stash before rebase of "feature/service" onto "main"

--- src/main/java/domain/ports/client/TaskApi.java
+++ src/main/java/domain/ports/client/TaskApi.java
@@ -3,13 +3,14 @@ package domain.ports.client;
 import domain.functional.model.Task;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface TaskApi {
-    Task createTask(String description) throws Exception;
+    void createTask(String description) throws Exception;
 
     void removeTask(Number id) throws Exception;
 
-    List<Task> retrieveAllTasks() throws Exception;
+    Optional<List<Task>> retrieveAllTasks() throws Exception;
 
-    Task markAsDone(Number id) throws Exception;
+    void markAsDone(Number id) throws Exception;
 }
